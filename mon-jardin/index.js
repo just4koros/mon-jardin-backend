10f111573393a2fabe7c9c809f872fbe2e9c7f57
just4koros <emmanuelkilel44@gmail.com>
2025-10-29 21:11:03 +0300

    Enable CORS for frontend access
    Removed duplicate cors import and usage in index.js for cleaner code.

--- mon-jardin/index.js
+++ mon-jardin/index.js
@@ -1,5 +1,6 @@
 const cors = require('cors');
 app.use(cors());
+
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -15,4 +16,5 @@ app.get('/', (req, res) => res.send('Mon Jardin ERP is running'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
